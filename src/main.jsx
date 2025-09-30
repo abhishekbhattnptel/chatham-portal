240532c8e50631796630cebb348c94e91de54c00
abhishekbhattnptel <abhishekbhattnptel@gmail.com>
2025-09-30 16:05:28 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Admin from "./Admin";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+function Root() {
+  const [hash, setHash] = React.useState(window.location.hash);
+  React.useEffect(() => {
+    const onHash = () => setHash(window.location.hash);
+    window.addEventListener("hashchange", onHash);
+    return () => window.removeEventListener("hashchange", onHash);
+  }, []);
+  if (hash === "#/admin") return <Admin />;
+  return <App />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
